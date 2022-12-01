cbf4485cb11a66ebed0ac0d5ea8e03d8ba24455a
samirizwan1999 <64922785+samirizwan1999@users.noreply.github.com>
2022-12-01 18:58:48 +0500

    first commit

---
+++ src/components/Annoucementbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Annoucementbar = () => {
+  return (
+    <div>
+      <section className="annoucment">
+            <p>Turo is open for business in Australia! Tap to explore cars.</p>
+      </section>
+    </div>
+  )
+}
+
+export default Annoucementbar
