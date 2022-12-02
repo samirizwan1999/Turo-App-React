9ee016f770fdeb0a24aecd4fb5fb19dfc38676ce
samirizwan1999 <64922785+samirizwan1999@users.noreply.github.com>
2022-12-03 02:01:42 +0500

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import banner from "./images/banner.jpg"
+import { Input } from 'antd';
+import { DatePicker, Space } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
+const dateFormat = 'YYYY/MM/DD';
 const Banner = () => {
   return (
     <div>
         <section>
-            <img src={banner} width="100%" height="auto"></img>
+            <div className='banner'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-md-4'>
+                        <div className='search-filter'>
+                            <Input placeholder="City, Airport, Address and Hotel" />
+                        </div>
+                        <div className='date-pick'>
+                        <DatePicker defaultValue={dayjs('2015/01/01', dateFormat)} format={dateFormat} />
+                        </div>
+                        </div>
+                        <div className='col-md-4'></div>
+                        <div className='col-md-4'></div>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
   )
