8b784dd38a1a354d7ffd8f95c4f2d04fcbae41da
samirizwan1999 <64922785+samirizwan1999@users.noreply.github.com>
2022-12-03 00:55:42 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Annoucementbar from "./components/Annoucementbar"
+import Banner from "./components/Banner"
 
 function App() {
   return (
     <div>
       <Annoucementbar />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import banner from "./images/banner.jpg"
+
+const Banner = () => {
+  return (
+    <div>
+        <section>
+            <img src={banner} width="100%" height="auto"></img>
+        </section>
+    </div>
+  )
+}
+
+export default Banner
