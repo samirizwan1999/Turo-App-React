f127de3395bca2cb07d0835f1e1cf5a72ccdc2fb
samirizwan1999 <64922785+samirizwan1999@users.noreply.github.com>
2022-12-03 12:30:26 +0500

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Annoucementbar from "./components/Annoucementbar"
 import Banner from "./components/Banner"
+import Title from "./components/Title"
 
 function App() {
   return (
     <div>
       <Annoucementbar />
       <Banner />
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>
+      <section className='find'>
+        <div className='container'>
+            <div className='your-drive'>
+                <h1>Find your drive</h1>
+                <div className='resting'>
+                <p>Explore the world's largest car sharing market place</p>
+                <div className='rest'>
+
+                </div>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Title
