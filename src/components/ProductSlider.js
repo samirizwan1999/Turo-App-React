137fcea5c837c233fe1de2e7503829d26f687658
samirizwan1999 <64922785+samirizwan1999@users.noreply.github.com>
2022-12-03 15:00:46 +0500

    update

--- src/components/ProductSlider.js
+++ src/components/ProductSlider.js
@@ -10,6 +10,10 @@ import Porsche from "./images/porsche.jpg"
 import Subaru from "./images/subaru.jpg"
 import Tesla from "./images/tesla.jpg"
 import Toyota from "./images/toyota.jpg"
+import {
+  LeftOutlined,
+  RightOutlined
+} from '@ant-design/icons';
 
 export default class SimpleSlider extends Component {
   render() {
@@ -17,43 +21,107 @@ export default class SimpleSlider extends Component {
       dots: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 1500,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        },
+        {
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        },
+        {
+          breakpoint: 850,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 2,
+          },
+        },
+      ],
     };
     return (
       <div className="slider-1">
         <div className="container">
-        <h2> Browse by make</h2>
+          <div className="head-with-arrows">
+           <h2> Browse by make</h2>
+          </div>
+          <LeftOutlined />
+          <RightOutlined />
         <Slider {...settings}>
-          <div>
+          <div className="car-slide">
             <img src={BMW} />
+            <p>BMW</p>
           </div>
-          <div>
+          <div className="car-slide">
             <img src={Ford} />
+            <p>Ford</p>
           </div>
-          <div>
+          <div className="car-slide">
             <img src={Jeep} />
+            <p>Jeep</p>
           </div>
-          <div>
+          <div className="car-slide">
           <img src={Lamborghini} />
+          <p>Lamborghini</p>
           </div>
-          <div>
+          <div className="car-slide">
           <img src={Mercedes} />
+          <p>Mercedes</p>
           </div>
-          <div>
+          <div className="car-slide">
           <img src={Nissan} />
           </div>
-          <div>
+          <div className="car-slide">
           <img src={Porsche} />
+          <p>Porsche</p>
           </div>
-          <div>
+          <div className="car-slide">
           <img src={Subaru} />
+          <p>Subaru</p>
           </div>
-          <div>
+          <div className="car-slide">
           <img src={Tesla} />
+          <p>Tesla</p>
           </div>
-          <div>
+          <div className="car-slide">
           <img src={Toyota} />
+          <p>Toyota</p>
           </div>
         </Slider>
         </div>
