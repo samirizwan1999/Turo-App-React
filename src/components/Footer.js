87e13f17faed5d9385af48dd1cb35c279b4655a4
samirizwan1999 <64922785+samirizwan1999@users.noreply.github.com>
2022-12-04 18:31:11 +0500

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
+        <div className='footer'>
         <div className='container'>
         <p className='footer-text'>
          * Any personal insurance you may have that covers damage to the host’s vehicle would kick in before your protection plan, except in limited situations for trips booked in Maryland, but this protects your own wallet. Liability insurance is provided under a policy issued to Turo by Travelers Excess and Surplus Lines Company. Terms, conditions, and exclusions apply. The policy does not provide coverage for damage to a host’s vehicle.
@@ -10,7 +11,60 @@ const Footer = () => {
            When a trip is booked in the state of Washington, physical damage to the host’s vehicle is covered by insurance purchased by Turo, but the Turo insurance does not change the contractual responsibilities of hosts or guests with respect to physical damage to a host’s vehicle.
          ** Terms, conditions, and exclusions apply.
          </p>
+         <div className='footer-menu'>
+            <div className='row'>
+                <div className='col-md-10'>
+                    <div className='row'>
+                        <div className='col-md-3'>
+                            <h2>Turo</h2>
+                            <ul>
+                                <li>About</li>
+                                <li>Team</li>
+                                <li>Policies</li>
+                                <li>Careers</li>
+                                <li>Press</li>
+                                <li>OpenRoad</li>
+                                <li>Turo shop</li>
+                            </ul>
+                        </div>
+                        <div className='col-md-3'>
+                        <h2>Locations</h2>
+                            <ul>
+                                <li>USA (EN)</li>
+                                <li>Canada (EN)</li>
+                                <li>Canada (FR)</li>
+                                <li>UK (EN)</li>
+                            </ul>
+                        </div>
+                        <div className='col-md-3'>
+                        <h2>Explore</h2>
+                            <ul>
+                                <li>Book a car</li>
+                                <li>Weddings</li>
+                                <li>Trust & safety</li>
+                                <li>Sustainability</li>
+                                <li>FAQs</li>
+                                <li>Get help</li>
+                            </ul>
+                        </div>
+                        <div className='col-md-3'>
+                        <h2>Hosting</h2>
+                            <ul>
+                                <li>List your car</li>
+                                <li>Carculator</li>
+                                <li>All-Star Hosts</li>
+                                <li>Host tools</li>
+                                <li>Insurance & protection</li>
+                                <li>FAQs</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-2'></div>
+            </div>
          </div>
+         </div>
+    </div>
     </div>
   )
 }
