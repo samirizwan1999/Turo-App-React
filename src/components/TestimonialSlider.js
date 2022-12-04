faa87dde9802dda6610de1f2761b5a87dea46dbb
samirizwan1999 <64922785+samirizwan1999@users.noreply.github.com>
2022-12-04 18:38:37 +0500

    update

--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -15,7 +15,57 @@ export default class SimpleSlider extends Component {
       infinite: true,
       speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1500,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        },
+        {
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        },
+        {
+          breakpoint: 850,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            // infinite: true,
+          },
+        }, 
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 2,
+          },
+        },
+      ],
     };
     return (
       <div className="container">
